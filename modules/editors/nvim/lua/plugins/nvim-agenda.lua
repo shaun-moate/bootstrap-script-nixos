782674b9ca081c181c8598145577b766eaa93342
shaun-moate <shaun.moate@gmail.com>
2023-05-23 15:18:58 +0100

    Neovim.Agenda: added keywords config

--- modules/editors/nvim/lua/plugins/nvim-agenda.lua
+++ modules/editors/nvim/lua/plugins/nvim-agenda.lua
@@ -1,6 +1,12 @@
 vim.opt.runtimepath:append("/home/smoate/code/nvim-agenda")
 
 require'nvim-agenda'.setup({
+  keywords = {
+    SUSPEND = { icon = "󱍥 ", color = "AgendaRed" },
+    TODO = { icon = " ", color = "AgendaYellow" },
+    FOCUS = { icon = "󰈸 ", color = "AgendaOrange" },
+    DONE = { icon = " ", color = "AgendaGreen" },
+  },
   theme = "gruvbox"
 })
 
