56bfed44e97a8780778af3e718f00bfada8b0d7e
shaun-moate <shaun.moate@gmail.com>
2023-05-24 00:17:13 +0100

    Neovim.Agenda: added telescope support and keymap

--- modules/editors/nvim/lua/plugins/nvim-agenda.lua
+++ modules/editors/nvim/lua/plugins/nvim-agenda.lua
@@ -7,6 +7,8 @@ require'nvim-agenda'.setup({
     FOCUS = { icon = "󰈸 ", color = "AgendaOrange" },
     DONE = { icon = " ", color = "AgendaGreen" },
   },
-  theme = "gruvbox"
+  theme = "gruvbox",
+  find_lines_keywords = { "SUSPEND", "TODO", "FOCUS" }
 })
 
+vim.api.nvim_set_keymap("n", "ft", ":AgendaTelescope<CR>", {})
