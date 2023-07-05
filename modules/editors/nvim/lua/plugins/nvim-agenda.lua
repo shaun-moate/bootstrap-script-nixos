b0e3cc4dd05d11810b794ad7800c543771837c30
shaun-moate <shaun.moate@gmail.com>
2023-07-05 09:38:41 +0100

    Nvim.Agenda: added toggle config

--- modules/editors/nvim/lua/plugins/nvim-agenda.lua
+++ modules/editors/nvim/lua/plugins/nvim-agenda.lua
@@ -7,12 +7,11 @@ require'nvim-agenda'.setup({
     FOCUS = { icon = "󰈸 ", color = "AgendaOrange", telescope_color = "AgendaOrangeTransparentBg" },
     DONE = { icon = " ", color = "AgendaGreen", telescope_color = "AgendaGreenTransparentBg" },
   },
-  telescope_keywords = { "SUSPEND", "TODO", "FOCUS" },
   theme = "gruvbox",
 })
 
-vim.api.nvim_set_keymap("n", "ft", ":AgendaTelescope<CR>", {})
-vim.api.nvim_set_keymap("n", "mtt", ":AgendaToggle to=TODO<CR>", {})
-vim.api.nvim_set_keymap("n", "mtf", ":AgendaToggle to=FOCUS<CR>", {})
-vim.api.nvim_set_keymap("n", "mts", ":AgendaToggle to=SUSPEND<CR>", {})
-vim.api.nvim_set_keymap("n", "mtd", ":AgendaToggle to=DONE<CR>", {})
+vim.api.nvim_set_keymap("n", "ft", ":AgendaTelescope find=TODO,SUSPEND,FOCUS<cr>", {})
+vim.api.nvim_set_keymap("n", "mtt", ":AgendaToggle to=TODO<cr>", {})
+vim.api.nvim_set_keymap("n", "mtf", ":AgendaToggle to=FOCUS<cr>", {})
+vim.api.nvim_set_keymap("n", "mts", ":AgendaToggle to=SUSPEND<cr>", {})
+vim.api.nvim_set_keymap("n", "mtd", ":AgendaToggle to=DONE<cr>", {})
